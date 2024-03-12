110fd4bb26e2136b6f15e348e1f9ad5b16125b22
Narguini <161517812+Narguini@users.noreply.github.com>
2024-03-12 23:11:22 +0000

    added tailwind styling

--- includes/nav.php
+++ includes/nav.php
@@ -1,19 +1,41 @@
-<nav class="navtop">
-			<div>
-				<h1>Avaliação de PAPs</h1>
-                <a href="home.php"><i class="fa-solid fa-home"></i></a>
+    <nav aria-label="navigation" class="container mx-auto flex max-w-screen-2xl items-center justify-between p-4 bg-[#0B090A]">
+        <a aria-label="logo" href="/" class="inline-flex text-white items-center justify-center rounded-lg">
+            <span class="sr-only">Logo</span>
+            Project
+        </a>
+
+        <ul class="flex items-center gap-2 text-sm font-medium text-white">
+        
+            <li class="rounded-lg px-3 py-2 hover:text-green-500 ease-in duration-200 hover:cursor-pointer" aria-label="projects">
+                <a href="/home.php" class="flex gap-1 items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7z"/></svg>
+                    Home
+                <a>
+            </li>
 
             <?php if($_SESSION['cargo'] == 'admin') :?>
-                <a href="#"><i class="fa-solid fa-plus"></i>Manage</a>
+                <li class="rounded-lg px-3 py-2 hover:text-green-500 ease-in duration-200 hover:cursor-pointer" aria-label="projects">
+                    <a href="/manage.php" class="flex items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"><path fill="currentColor" d="M2 18v-.8q0-.825.425-1.55t1.175-1.1q1.275-.65 2.875-1.1T10 13h.35q.15 0 .3.05q-.725 1.8-.6 3.575T11.25 20H4q-.825 0-1.412-.587T2 18m15 0q.825 0 1.413-.587T19 16q0-.825-.587-1.412T17 14q-.825 0-1.412.588T15 16q0 .825.588 1.413T17 18m-7-6q-1.65 0-2.825-1.175T6 8q0-1.65 1.175-2.825T10 4q1.65 0 2.825 1.175T14 8q0 1.65-1.175 2.825T10 12m5.85 8.2l-.15-.7q-.3-.125-.562-.262T14.6 18.9l-.725.225q-.325.1-.637-.025t-.488-.4l-.2-.35q-.175-.3-.125-.65t.325-.575l.55-.475q-.05-.35-.05-.65t.05-.65l-.55-.475q-.275-.225-.325-.563t.125-.637l.225-.375q.175-.275.475-.4t.625-.025l.725.225q.275-.2.538-.338t.562-.262l.15-.725q.075-.35.338-.562T16.8 11h.4q.35 0 .613.225t.337.575l.15.7q.3.125.562.275t.538.375l.675-.225q.35-.125.675 0t.5.425l.2.35q.175.3.125.65t-.325.575l-.55.475q.05.3.05.625t-.05.625l.55.475q.275.225.325.563t-.125.637l-.225.375q-.175.275-.475.4t-.625.025L19.4 18.9q-.275.2-.538.337t-.562.263l-.15.725q-.075.35-.337.563T17.2 21h-.4q-.35 0-.612-.225t-.338-.575"/></svg>
+                        Manage
+                    </li>
             <?php endif; ?>
 
-            <?php if($_SESSION['cargo'] == 'professor' || $_SESSION['cargo'] == 'admin') :?>
-                <a href="post.php"><i class="fa-solid fa-plus"></i>Novo projeto</a>
+            <?php if($_SESSION['cargo'] == 'professor' || $_SESSION['cargo'] == 'admin') :?> 
+                <li class="rounded-lg px-3 py-2 hover:text-green-500 ease-in duration-200 hover:cursor-pointer" aria-label="about">
+                    <a href="/post.php" class="flex items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"><path fill="currentColor" d="M11 17h2v-4h4v-2h-4V7h-2v4H7v2h4zm1 5q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20m0-8"/></svg>
+                        Add project
+                    </a>
+                </li>
             <?php endif; ?>
 
-				<a href="actions/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
+            <li class="rounded-lg px-3 py-2 hover:text-green-500 ease-in duration-200 hover:cursor-pointer" aria-label="contact">
+                <a href="actions/logout.php" class="flex items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"><path fill="currentColor" d="m17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4z"/></svg>
+                    Logout
+                </a>
+                </li>
 
-
-
-</nav>
+        </ul>
+    </nav>
--- post.php
+++ post.php
@@ -7,8 +7,19 @@ session_start();
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
-    
+
+
 }
+$stmt = $conn->prepare("SELECT * FROM paps");
+if(!$stmt) {
+	echo "Error: " . $conn->error;
+	exit;
+}
+if(!$stmt->execute()) {
+	echo "Error: " . $stmt->error;
+	exit;
+  }
+$projeto = $stmt->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -17,48 +28,72 @@ if (!isset($_SESSION['loggedin'])) {
 		<meta charset="utf-8">
 		<title>Página Inicial</title>
 		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<script defer src="https://cdn.tailwindcss.com"></script>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Avaliação de PAPs</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Perfil</a>
-				<a href="actions/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+		<?php include('includes/nav.php'); ?>
+		<div class="container mx-auto p-4 mt-12">
+			<div class="grid grid-cols-3 gap-6">
+				<div class="col-span-1">
+					<h1 class="text-white text-2xl font-semibold">Create a new project</h1>
+					<p class="text-gray-400 mt-3 font-medium">
+						<!-- some description that summarizes the forms purpose here -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+						Iste dolorem earum quis corrupti at deleniti optio vel consequatur, 
+						laborum esse autem placeat incidunt amet expedita aut, enim eos non velit.
+  						<br/><br/>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+						Iste dolorem earum quis corrupti at deleniti optio vel consequatur, 
+						laborum esse autem placeat incidunt amet expedita aut, enim eos non velit.
+					</p>
+				</div>
+				<div class="col-span-2">
+					<form class="flex flex-col gap-y-5" action="actions/insertProject.php" method="POST">
+						<div class="flex flex-row gap-x-3 items-center">
+							<div class="flex flex-col gap-3 w-2/5">
+								<label class="text-white font-medium" for="titulo">Titulo:</label>
+								<input type="text" id="titulo" name="titulo" placeholder="enter a title"  class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0">
+							</div>
+
+							<div class="flex flex-col gap-3 w-2/5">
+								<label class="text-white font-medium" for="aluno1">Nome do Aluno:</label>
+								<input type="text" id="aluno1" name="aluno1" class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0">
+							</div>
+
+							<div class="flex flex-col gap-3 w-2/5">
+								<label class="text-white font-medium" for="aluno1">Nome do Aluno:</label>
+								<input type="text" id="aluno2" name="aluno2" class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0">
+							</div>
+				
+						</div>
+						<div class="flex flex-row gap-x-3 items-center">
+							<div class="flex flex-col gap-3 w-2/5">
+								<label class="text-white font-medium" for="nome_professor">Nome do Professor:</label>
+								<input type="text" id="nome_professor" name="nome_professor" class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0">
+							</div>
+							<div class="flex flex-col gap-3 w-2/5">
+								<label class="text-white font-medium" for="data_inicio">Data de Início:</label>
+								<input type="date" id="data_inicio" name="data_inicio" class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0">
+							</div>
+
+							<div class="flex flex-col gap-3 w-2/5">
+								<label class="text-white font-medium" for="data_entrega">Data de Entrega:</label>
+								<input type="date" id="data_entrega" name="data_entrega" class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0">
+							</div>
+						</div>
+
+						<div class="flex flex-col gap-3">
+							<label  class="text-white font-medium" for="descricao">Descrição:</label>
+							<textarea rows="4" id="descricao" name="descricao" class="py-2 px-2 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm outline-none focus:ring-0"></textarea>
+						</div>
+						
+						<button type="button" class="bg-blue-500 hover:bg-blue-600 transition-all duration-150 ease-in-out w-1/5 ml-auto py-2 px-3 text-white font=medium rounded-md ">
+							Publicar
+						</button>
+					</form>
+				</div>
 			</div>
-		</nav>
-		<form action="actions/insertProject.php" method="POST">
-       
-        <label for="titulo">Titulo:</label><br>
-        <input type="text" id="titulo" name="titulo"><br>
-
-        <label for="descricao">Descrição:</label><br>
-        <textarea id="descricao" name="descricao"></textarea><br><br>
- 
-        <label for="aluno1">Nome do Aluno:</label><br>
-        <input type="text" id="aluno1" name="aluno1"><br><br>
- 
-		<label for="aluno2">Nome do segundo Aluno:</label><br>
-        <input type="text" id="aluno2" name="aluno2"><br><br>
-
-        <label for="nome_professor">Nome do Professor:</label><br>
-        <input type="text" id="nome_professor" name="nome_professor"><br><br>
- 
-        <label for="data_inicio">Data de Início:</label><br>
-        <input type="date" id="data_inicio" name="data_inicio"><br><br>
- 
-        <label for="data_entrega">Data de Entrega:</label><br>
-        <input type="date" id="data_entrega" name="data_entrega"><br><br>
- 
-        <label for="avaliacao_final">Avaliação Final:</label><br>
-        <input type="number" id="avaliacao_final" name="avaliacao_final"><br><br>
- 
-        <label for="myfile">Select a file:</label>
-        <input type="file" id="myfile" name="myfile"><br><br>
-
-        <input type="submit" value="Publicar">
-
-      </form>
 		</div>
 	</body>
 </html>
